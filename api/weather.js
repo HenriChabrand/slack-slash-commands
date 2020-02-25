3d684853f2bc1ffd95832ef0107ae39fbcea93f0
Henri <HenriChabrand@users.noreply.github.com>
2020-02-25 13:32:50 +0100

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -16,15 +16,59 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     const data = getPayLoad(req);
     const response = {
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Hi David :wave:"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Great to see you here! App helps you to stay up-to-date with your meetings and events right here within Slack. These are just a few things which you will be able to do:"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "• Schedule meetings \n • Manage and update attendees \n • Get notified about changes of your meetings"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "But before you can do all these amazing things, we need you to connect your calendar to App. Simply click the button below:"
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Connect account",
+                                "emoji": true
+                            },
+                            "value": "click_me_123"
+                        }
+                    ]
+                }
+	        ]
+    };
+    res.send(response);
+});
+/*{
         "response_type": "in_channel",
         "text": "Now: 34 degrees and raining",
         "attachments": [{
             "text":`Thanks for asking ${data.user_name}. Hi everyone! :wave:`
-        }]
-    };
-    res.send(response);
-});
-
+        }*/
 /*
     The following data is available (see https://api.slack.com/slash-commands)
     token, team_id, team_domain, channel_id, channel_name, user_id, user_name,
